9fbabe390ba67114622ea202875aa5835939b819
Fadzri Syarif <41849638+fadzrisyarif@users.noreply.github.com>
2023-03-21 10:30:33 +0700

    second commit

---
+++ out/app/code0.js
@@ -0,0 +1,21 @@
+gdjs.Main_32MenuCode = {};
+
+gdjs.Main_32MenuCode.conditionTrue_0 = {val:false};
+gdjs.Main_32MenuCode.condition0IsTrue_0 = {val:false};
+
+
+gdjs.Main_32MenuCode.eventsList0 = function(runtimeScene) {
+
+};
+
+gdjs.Main_32MenuCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs.Main_32MenuCode.eventsList0(runtimeScene);
+
+return;
+
+}
+
+gdjs['Main_32MenuCode'] = gdjs.Main_32MenuCode;
